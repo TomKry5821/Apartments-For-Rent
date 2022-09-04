dc1a53392b5138b0bf68562c4da0f4567c7ffe2f
TomKry5821 <tomakry729@student.polsl.pl>
2022-09-04 20:49:23 +0200

    Architecture reorganisation

---
+++ Announcement-service/src/main/java/pl/polsl/krypczyk/apartmentsforrent/announcementservice/TestController.java
@@ -0,0 +1,15 @@
+package pl.polsl.krypczyk.apartmentsforrent.announcementservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/announcement/api/v1/test")
+public class TestController {
+
+    @GetMapping()
+    public String test() {
+        return "Announcement controller check";
+    }
+}
---
+++ Announcement-service/src/test/java/pl/polsl/krypczyk/apartmentsforrent/announcementservice/AnnouncementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.polsl.krypczyk.apartmentsforrent.announcementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnnouncementServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
