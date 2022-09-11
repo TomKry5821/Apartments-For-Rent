940453ee1a94ce8703705608c70a8b6cb9b683e9
TomKry5821 <tomakry729@student.polsl.pl>
2022-09-11 14:41:37 +0200

    Added @Builder

--- User-service/src/main/java/pl/polsl/krypczyk/apartmentsforrent/userservice/user/userdetails/dto/ChangeUserDetailsDTO.java
+++ User-service/src/main/java/pl/polsl/krypczyk/apartmentsforrent/userservice/user/userdetails/dto/ChangeUserDetailsDTO.java
@@ -1,11 +1,13 @@
 package pl.polsl.krypczyk.apartmentsforrent.userservice.user.userdetails.dto;
 
+import lombok.Builder;
 import lombok.Data;
 
 import javax.validation.constraints.NotNull;
 
 @Data
 @NotNull
+@Builder
 public class ChangeUserDetailsDTO {
 
     @NotNull(message = "User id cannot be null")
