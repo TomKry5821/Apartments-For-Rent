240d620727f17ea7a388690a1cd55abd70e9c8a9
TomKry5821 <tomakry729@student.polsl.pl>
2022-09-12 23:19:55 +0200

    Package refactored

--- User-service/src/main/java/pl/polsl/krypczyk/apartmentsforrent/userservice/application/admin/AdminController.java
+++ User-service/src/main/java/pl/polsl/krypczyk/apartmentsforrent/userservice/application/admin/AdminController.java
@@ -1,10 +1,10 @@
-package pl.polsl.krypczyk.apartmentsforrent.userservice.application.user;
+package pl.polsl.krypczyk.apartmentsforrent.userservice.application.admin;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import pl.polsl.krypczyk.apartmentsforrent.userservice.domain.admin.AdminService;
 import pl.polsl.krypczyk.apartmentsforrent.userservice.domain.authorization.AuthorizationService;
-import pl.polsl.krypczyk.apartmentsforrent.userservice.domain.user.UserService;
 import pl.polsl.krypczyk.apartmentsforrent.userservice.infrastructure.annotation.uuid;
 
 import javax.validation.constraints.NotNull;
@@ -15,7 +15,7 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class AdminController {
 
-    private final UserService userService;
+    private final AdminService adminService;
     private final AuthorizationService authorizationService;
 
     @DeleteMapping("/users/{userId}")
@@ -23,6 +23,6 @@ public class AdminController {
     public void DeleteUser(@PathVariable("userId") @NotNull Long userId,
                            @RequestHeader("Authorization") @uuid UUID accessToken) {
         this.authorizationService.authorizeAdmin(accessToken);
-        this.userService.deleteUser(userId);
+        this.adminService.deleteUser(userId);
     }
 }
