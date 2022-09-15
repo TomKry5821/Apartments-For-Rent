2473180f61a24c23cd7d34beb3cd84dce10abd8e
TomKry5821 <tomakry729@student.polsl.pl>
2022-09-15 19:17:10 +0200

    Added photo path entity

---
+++ Announcement-service/src/main/java/pl/polsl/krypczyk/apartmentsforrent/announcementservice/domain/announcementphoto/PhotoPathEntity.java
@@ -0,0 +1,25 @@
+package pl.polsl.krypczyk.apartmentsforrent.announcementservice.domain.announcementphoto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+@ToString
+@Entity
+@Table(name = "PHOTO_PATH")
+public class PhotoPathEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID", nullable = false)
+    private Long id;
+
+    @Column(name = "PHOTO_PATH", nullable = false)
+    private String photoPath;
+}
