3d7ae1129a42407f0dac8e4538ec1605c3478462
TomKry5821 <tomakry729@student.polsl.pl>
2022-09-15 20:50:41 +0200

    Added creation date field

--- Announcement-service/src/main/java/pl/polsl/krypczyk/apartmentsforrent/announcementservice/domain/announcement/AnnouncementEntity.java
+++ Announcement-service/src/main/java/pl/polsl/krypczyk/apartmentsforrent/announcementservice/domain/announcement/AnnouncementEntity.java
@@ -7,6 +7,7 @@ import lombok.ToString;
 import pl.polsl.krypczyk.apartmentsforrent.announcementservice.domain.announcementdetails.AnnouncementDetailsEntity;
 
 import javax.persistence.*;
+import java.time.LocalDate;
 
 @Getter
 @Setter
@@ -23,6 +24,9 @@ public class AnnouncementEntity {
 
     @Column(name = "USER_ID", nullable = false)
     private Long userId;
+
+    @Column(name = "CREATION_DATE", nullable = false)
+    private LocalDate creationDate;
 
     @OneToOne(orphanRemoval = true, optional = false, fetch = FetchType.EAGER)
     @JoinColumn(name = "ANNOUNCEMENT_DETAILS_ID", referencedColumnName = "ID")
--- Announcement-service/src/main/java/pl/polsl/krypczyk/apartmentsforrent/announcementservice/domain/announcement/dto/AnnouncementDTO.java
+++ Announcement-service/src/main/java/pl/polsl/krypczyk/apartmentsforrent/announcementservice/domain/announcement/dto/AnnouncementDTO.java
@@ -3,11 +3,15 @@ package pl.polsl.krypczyk.apartmentsforrent.announcementservice.domain.announcem
 import lombok.Data;
 import pl.polsl.krypczyk.apartmentsforrent.announcementservice.domain.announcementdetails.dto.AnnouncementDetailsDTO;
 
+import java.time.LocalDate;
+
 
 @Data
 public class AnnouncementDTO {
 
     private Long userId;
+
+    private LocalDate creationDate;
 
     private AnnouncementDetailsDTO announcementDetailsDTO;
 }
