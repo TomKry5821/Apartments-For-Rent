d83132efaf7e9a15f3a1e4bec099231317192a4a
TomKry5821 <tomakry729@student.polsl.pl>
2022-09-16 15:49:38 +0200

    Added enabled transactional configuration

--- Announcement-service/src/main/java/pl/polsl/krypczyk/apartmentsforrent/announcementservice/AnnouncementServiceApplication.java
+++ Announcement-service/src/main/java/pl/polsl/krypczyk/apartmentsforrent/announcementservice/AnnouncementServiceApplication.java
@@ -2,8 +2,10 @@ package pl.polsl.krypczyk.apartmentsforrent.announcementservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class AnnouncementServiceApplication {
 
     public static void main(String[] args) {
