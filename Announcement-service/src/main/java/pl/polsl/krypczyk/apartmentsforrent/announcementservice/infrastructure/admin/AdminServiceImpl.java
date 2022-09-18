c74ed76879cbddf945a470190c1ec8fc67e608ae
TomKry5821 <tomakry729@student.polsl.pl>
2022-09-18 13:42:12 +0200

    Added clearing photopaths

--- Announcement-service/src/main/java/pl/polsl/krypczyk/apartmentsforrent/announcementservice/infrastructure/admin/AdminServiceImpl.java
+++ Announcement-service/src/main/java/pl/polsl/krypczyk/apartmentsforrent/announcementservice/infrastructure/admin/AdminServiceImpl.java
@@ -2,6 +2,7 @@ package pl.polsl.krypczyk.apartmentsforrent.announcementservice.infrastructure.a
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import pl.polsl.krypczyk.apartmentsforrent.announcementservice.domain.admin.AdminService;
 import pl.polsl.krypczyk.apartmentsforrent.announcementservice.domain.announcement.AnnouncementRepository;
 import pl.polsl.krypczyk.apartmentsforrent.announcementservice.domain.announcement.excpetion.AnnouncementNotFoundException;
@@ -13,11 +14,12 @@ public class AdminServiceImpl implements AdminService {
     private final AnnouncementRepository announcementRepository;
 
     @Override
+    @Transactional
     public void deleteAnnouncement(Long announcementId) throws AnnouncementNotFoundException {
         var announcement = this.announcementRepository.findById(announcementId);
         if(announcement.isEmpty())
             throw new AnnouncementNotFoundException();
-
+        announcement.get().getAnnouncementDetailsEntity().getAnnouncementContent().getPhotoPaths().clear();
         this.announcementRepository.delete(announcement.get());
     }
 }
