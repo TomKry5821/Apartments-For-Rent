42274b4eca386cba3a29aa081c9ef939460bf0c7
TomKry5821 <tomakry729@student.polsl.pl>
2022-09-18 13:21:16 +0200

    Added orphan removal to photo paths

--- Announcement-service/src/main/java/pl/polsl/krypczyk/apartmentsforrent/announcementservice/domain/announcementcontent/AnnouncementContentEntity.java
+++ Announcement-service/src/main/java/pl/polsl/krypczyk/apartmentsforrent/announcementservice/domain/announcementcontent/AnnouncementContentEntity.java
@@ -24,7 +24,7 @@ public class AnnouncementContentEntity {
     @Column(name = "CONTENT", nullable = false)
     private String content;
 
-    @OneToMany(fetch = FetchType.EAGER)
+    @OneToMany(fetch = FetchType.EAGER, orphanRemoval = true)
     private Collection<PhotoPathEntity> photoPaths;
 
 }
