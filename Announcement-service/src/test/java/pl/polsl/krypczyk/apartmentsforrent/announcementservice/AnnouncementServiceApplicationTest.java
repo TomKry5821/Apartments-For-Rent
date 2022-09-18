d49aee0da85d857d8019d95793092ca70b4cace2
TomKry5821 <tomakry729@student.polsl.pl>
2022-09-18 14:12:56 +0200

    Added test

---
+++ Announcement-service/src/test/java/pl/polsl/krypczyk/apartmentsforrent/announcementservice/AnnouncementServiceApplicationTest.java
@@ -0,0 +1,12 @@
+package pl.polsl.krypczyk.apartmentsforrent.announcementservice;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AnnouncementServiceApplicationTest {
+
+    @Test
+    void main() {
+    }
+}
