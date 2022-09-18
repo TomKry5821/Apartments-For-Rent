b4f4f5ccd2319849ba9497bacacf1fce32e393ef
TomKry5821 <tomakry729@student.polsl.pl>
2022-09-18 12:33:30 +0200

    Added delete announcement logic implementation

---
+++ Announcement-service/src/main/java/pl/polsl/krypczyk/apartmentsforrent/announcementservice/application/admin/AdminController.java
@@ -0,0 +1,24 @@
+package pl.polsl.krypczyk.apartmentsforrent.announcementservice.application.admin;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import pl.polsl.krypczyk.apartmentsforrent.announcementservice.domain.admin.AdminService;
+import pl.polsl.krypczyk.apartmentsforrent.announcementservice.domain.announcement.excpetion.AnnouncementNotFoundException;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
+@RestController
+@RequestMapping("announcement/api/v1/admin")
+@RequiredArgsConstructor
+public class AdminController {
+
+    private final AdminService adminService;
+
+    @DeleteMapping("announcements/{announcementId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteAnnouncement(@PathVariable("announcementId") @NotNull @Min(value = 1) Long announcementId) throws AnnouncementNotFoundException {
+        this.adminService.deleteAnnouncement(announcementId);
+    }
+}
