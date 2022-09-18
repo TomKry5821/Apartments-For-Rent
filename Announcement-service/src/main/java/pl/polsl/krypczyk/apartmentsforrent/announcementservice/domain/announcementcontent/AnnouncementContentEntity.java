3b4fe035ca1d85b43de403eb7899fcd67bff2f4b
TomKry5821 <tomakry729@student.polsl.pl>
2022-09-18 13:52:59 +0200

    Added cascade type for photo paths

--- Announcement-service/src/main/java/pl/polsl/krypczyk/apartmentsforrent/announcementservice/domain/announcementcontent/AnnouncementContentEntity.java
+++ Announcement-service/src/main/java/pl/polsl/krypczyk/apartmentsforrent/announcementservice/domain/announcementcontent/AnnouncementContentEntity.java
@@ -24,7 +24,7 @@ public class AnnouncementContentEntity {
     @Column(name = "CONTENT", nullable = false)
     private String content;
 
-    @OneToMany(fetch = FetchType.EAGER)
+    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
     private Collection<PhotoPathEntity> photoPaths;
 
 }
