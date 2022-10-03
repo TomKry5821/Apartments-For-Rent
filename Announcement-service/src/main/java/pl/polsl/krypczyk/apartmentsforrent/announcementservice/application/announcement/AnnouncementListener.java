330731433ba10458746c2818a32dde548098af6d
TomKry5821 <tomakry729@student.polsl.pl>
2022-10-03 17:22:40 +0200

    Corrected logic

--- Announcement-service/src/main/java/pl/polsl/krypczyk/apartmentsforrent/announcementservice/application/announcement/AnnouncementListener.java
+++ Announcement-service/src/main/java/pl/polsl/krypczyk/apartmentsforrent/announcementservice/application/announcement/AnnouncementListener.java
@@ -1,14 +1,18 @@
 package pl.polsl.krypczyk.apartmentsforrent.announcementservice.application.announcement;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
+import pl.polsl.krypczyk.apartmentsforrent.announcementservice.domain.announcement.AnnouncementService;
 
 @Component
+@RequiredArgsConstructor
 public class AnnouncementListener {
 
+    private final AnnouncementService announcementService;
     // IF DATA WILL BE OUR CUSTOM CLASS WE WILL PASS THERE THAT CLASS
-    @KafkaListener(topics = "topic", groupId = "test", containerFactory = "kafkaListenerContainerFactory")
-    void listener(String test) {
-        System.out.println(test);
+    @KafkaListener(topics = "topic", groupId = "test", containerFactory = "kafkaListenerWithUserIdContainerFactory")
+    void listener(Long userId) {
+        this.announcementService.closeUserAnnouncements(userId);
     }
 }
