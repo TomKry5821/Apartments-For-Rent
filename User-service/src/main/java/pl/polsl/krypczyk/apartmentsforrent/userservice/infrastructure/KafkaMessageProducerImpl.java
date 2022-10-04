6eb3089235999965e5d0d08cab82db09be6bebb7
TomKry5821 <tomakry729@student.polsl.pl>
2022-10-04 14:34:40 +0200

    Added delete user announcements message implementation

--- User-service/src/main/java/pl/polsl/krypczyk/apartmentsforrent/userservice/infrastructure/KafkaMessageProducerImpl.java
+++ User-service/src/main/java/pl/polsl/krypczyk/apartmentsforrent/userservice/infrastructure/KafkaMessageProducerImpl.java
@@ -11,14 +11,22 @@ import pl.polsl.krypczyk.apartmentsforrent.userservice.domain.KafkaMessageProduc
 @RequiredArgsConstructor
 public class KafkaMessageProducerImpl implements KafkaMessageProducer {
 
-    private final String USER_ANNOUNCEMENT_TOPIC = "user-announcement";
+    private final String INACTIVATE_ANNOUNCEMENT_TOPIC = "inactivate-announcement";
+    private final String DELETE_ANNOUNCEMENT_TOPIC = "delete-announcement";
     private final KafkaTemplate<String, Long> kafkaWithUserIdTemplate;
 
     @Override
     public void sendInactivateAnnouncementsMessage(Long userId) {
-        log.info("Sending inactivate announcements message for user with id " + userId + " on topic " + USER_ANNOUNCEMENT_TOPIC);
-        this.kafkaWithUserIdTemplate.send(USER_ANNOUNCEMENT_TOPIC, userId);
-        log.info("Successfully sent inactivate announcements message for user with id " + userId + " on topic " + USER_ANNOUNCEMENT_TOPIC);
+        log.info("Sending inactivate announcements message for user with id " + userId + " on topic " + INACTIVATE_ANNOUNCEMENT_TOPIC);
+        this.kafkaWithUserIdTemplate.send(INACTIVATE_ANNOUNCEMENT_TOPIC, userId);
+        log.info("Successfully sent inactivate announcements message for user with id " + userId + " on topic " + INACTIVATE_ANNOUNCEMENT_TOPIC);
+    }
+
+    @Override
+    public  void sendDeleteAnnouncementMessage(Long userId){
+        log.info("Sending delete announcements message for user with id " + userId + " on topic " + INACTIVATE_ANNOUNCEMENT_TOPIC);
+        this.kafkaWithUserIdTemplate.send(DELETE_ANNOUNCEMENT_TOPIC, userId);
+        log.info("Successfully sent deleyr announcements message for user with id " + userId + " on topic " + INACTIVATE_ANNOUNCEMENT_TOPIC);
     }
 
 }
