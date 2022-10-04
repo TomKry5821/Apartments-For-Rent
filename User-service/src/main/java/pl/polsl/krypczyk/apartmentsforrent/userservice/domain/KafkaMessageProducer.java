9897c2d6830c282b1206d211291ce29d9d7dedf4
TomKry5821 <tomakry729@student.polsl.pl>
2022-10-04 14:33:57 +0200

    Added delete user announcements message method

--- User-service/src/main/java/pl/polsl/krypczyk/apartmentsforrent/userservice/domain/KafkaMessageProducer.java
+++ User-service/src/main/java/pl/polsl/krypczyk/apartmentsforrent/userservice/domain/KafkaMessageProducer.java
@@ -3,4 +3,6 @@ package pl.polsl.krypczyk.apartmentsforrent.userservice.domain;
 public interface KafkaMessageProducer {
 
     void sendInactivateAnnouncementsMessage(Long userId);
+
+    void sendDeleteAnnouncementMessage(Long userId);
 }
