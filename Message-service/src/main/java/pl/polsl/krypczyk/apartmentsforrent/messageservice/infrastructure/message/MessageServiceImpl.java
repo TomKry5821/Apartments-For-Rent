9efddffc90b2cf43b058b7f923265caae24e28bc
TomKry5821 <tomakry729@student.polsl.pl>
2022-10-05 21:48:06 +0200

    Added message service implementation

--- Message-service/src/main/java/pl/polsl/krypczyk/apartmentsforrent/messageservice/infrastructure/message/MessageServiceImpl.java
+++ Message-service/src/main/java/pl/polsl/krypczyk/apartmentsforrent/messageservice/infrastructure/message/MessageServiceImpl.java
@@ -1,8 +1,34 @@
 package pl.polsl.krypczyk.apartmentsforrent.messageservice.infrastructure.message;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.mapstruct.factory.Mappers;
 import org.springframework.stereotype.Service;
+import pl.polsl.krypczyk.apartmentsforrent.messageservice.application.message.request.AddNewMessageRequest;
+import pl.polsl.krypczyk.apartmentsforrent.messageservice.application.message.response.AddNewMessageResponse;
+import pl.polsl.krypczyk.apartmentsforrent.messageservice.domain.EntityFactory;
+import pl.polsl.krypczyk.apartmentsforrent.messageservice.domain.ResponseFactory;
+import pl.polsl.krypczyk.apartmentsforrent.messageservice.domain.message.MessageMapper;
+import pl.polsl.krypczyk.apartmentsforrent.messageservice.domain.message.MessageRepository;
 import pl.polsl.krypczyk.apartmentsforrent.messageservice.domain.message.MessageService;
 
+
 @Service
+@RequiredArgsConstructor
+@Slf4j
 public class MessageServiceImpl implements MessageService {
+
+    private final ResponseFactory responseFactory;
+
+    private final EntityFactory entityFactory;
+
+    @Override
+    public AddNewMessageResponse addNewMessage(AddNewMessageRequest addNewMessageRequest) {
+        log.info("Started adding message - " + addNewMessageRequest);
+
+        var message = this.entityFactory.createMessageEntity(addNewMessageRequest);
+
+        log.info("Successfully added message - " + message);
+        return this.responseFactory.createAddNewMessageResponse(addNewMessageRequest, message.getId());
+    }
 }
