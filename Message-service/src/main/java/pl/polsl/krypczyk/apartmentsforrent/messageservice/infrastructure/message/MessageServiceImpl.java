60b0e3146e4db87cf8d16b5ca1fcde2423b3e824
TomKry5821 <tomakry729@student.polsl.pl>
2022-10-05 10:23:06 +0200

    Created message service implementation

---
+++ Message-service/src/main/java/pl/polsl/krypczyk/apartmentsforrent/messageservice/infrastructure/message/MessageServiceImpl.java
@@ -0,0 +1,8 @@
+package pl.polsl.krypczyk.apartmentsforrent.messageservice.infrastructure.message;
+
+import org.springframework.stereotype.Service;
+import pl.polsl.krypczyk.apartmentsforrent.messageservice.domain.message.MessageService;
+
+@Service
+public class MessageServiceImpl implements MessageService {
+}
