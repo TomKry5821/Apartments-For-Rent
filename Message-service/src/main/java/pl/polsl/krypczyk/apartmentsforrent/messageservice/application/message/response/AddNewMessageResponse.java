318678e0912f00968120b7bd270e04b6d50fc32b
TomKry5821 <tomakry729@student.polsl.pl>
2022-10-05 21:46:44 +0200

    Added add message response DTO

---
+++ Message-service/src/main/java/pl/polsl/krypczyk/apartmentsforrent/messageservice/application/message/response/AddNewMessageResponse.java
@@ -0,0 +1,20 @@
+package pl.polsl.krypczyk.apartmentsforrent.messageservice.application.message.response;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class AddNewMessageResponse {
+
+    private Long id;
+
+    private Long senderId;
+
+    private Long receiverId;
+
+    private String message;
+
+    private Integer attachmentsCount;
+
+}
