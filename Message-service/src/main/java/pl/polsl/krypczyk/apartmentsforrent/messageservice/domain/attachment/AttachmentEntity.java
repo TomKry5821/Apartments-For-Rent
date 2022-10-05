079f8fe1f35a595087a5f600f7866f2a742a600c
TomKry5821 <tomakry729@student.polsl.pl>
2022-10-05 10:22:18 +0200

    Created attachment entity

---
+++ Message-service/src/main/java/pl/polsl/krypczyk/apartmentsforrent/messageservice/domain/attachment/AttachmentEntity.java
@@ -0,0 +1,23 @@
+package pl.polsl.krypczyk.apartmentsforrent.messageservice.domain.attachment;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+@Entity
+@Table(name = "ATTACHMENT")
+public class AttachmentEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID", nullable = false)
+    private Long id;
+
+    @Column(name = "ATTACHMENT_PATH")
+    private String attachmentPath;
+}
