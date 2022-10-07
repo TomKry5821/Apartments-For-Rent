533f9425dbea753a4d394ed7d2d3616ee839568e
TomKry5821 <tomakry729@student.polsl.pl>
2022-10-07 22:29:40 +0200

    Added get conversation method

--- Message-service/src/main/java/pl/polsl/krypczyk/apartmentsforrent/messageservice/domain/message/MessageService.java
+++ Message-service/src/main/java/pl/polsl/krypczyk/apartmentsforrent/messageservice/domain/message/MessageService.java
@@ -2,8 +2,13 @@ package pl.polsl.krypczyk.apartmentsforrent.messageservice.domain.message;
 
 import pl.polsl.krypczyk.apartmentsforrent.messageservice.application.message.request.AddNewMessageRequest;
 import pl.polsl.krypczyk.apartmentsforrent.messageservice.application.message.response.AddNewMessageResponse;
+import pl.polsl.krypczyk.apartmentsforrent.messageservice.application.message.response.MessageDTO;
+
+import java.util.Collection;
 
 public interface MessageService {
 
     AddNewMessageResponse addNewMessage(AddNewMessageRequest addNewMessageRequest);
+
+    Collection<MessageDTO> getConversation(Long senderId, Long receiverId);
 }
