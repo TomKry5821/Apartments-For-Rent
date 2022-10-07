aed493f9fa89ff17118cc198742ebb8d8558fb54
TomKry5821 <tomakry729@student.polsl.pl>
2022-10-07 22:28:25 +0200

    Added create get conversation response method

--- Message-service/src/main/java/pl/polsl/krypczyk/apartmentsforrent/messageservice/domain/ResponseFactory.java
+++ Message-service/src/main/java/pl/polsl/krypczyk/apartmentsforrent/messageservice/domain/ResponseFactory.java
@@ -3,9 +3,15 @@ package pl.polsl.krypczyk.apartmentsforrent.messageservice.domain;
 
 import pl.polsl.krypczyk.apartmentsforrent.messageservice.application.message.request.AddNewMessageRequest;
 import pl.polsl.krypczyk.apartmentsforrent.messageservice.application.message.response.AddNewMessageResponse;
+import pl.polsl.krypczyk.apartmentsforrent.messageservice.application.message.response.MessageDTO;
+import pl.polsl.krypczyk.apartmentsforrent.messageservice.domain.message.MessageEntity;
+
+import java.util.Collection;
 
 public interface ResponseFactory {
 
     AddNewMessageResponse createAddNewMessageResponse(AddNewMessageRequest addNewMessageRequest, Long messageId);
+
+    Collection<MessageDTO> createGetConversationResponse(Collection<MessageEntity> messageEntities);
 
 }
