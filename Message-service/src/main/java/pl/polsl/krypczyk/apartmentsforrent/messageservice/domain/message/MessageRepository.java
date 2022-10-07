6af847fc5b8dac698e28d05487a8f688bfc40030
TomKry5821 <tomakry729@student.polsl.pl>
2022-10-07 22:29:24 +0200

    Added get entities by sender and receiver id method

--- Message-service/src/main/java/pl/polsl/krypczyk/apartmentsforrent/messageservice/domain/message/MessageRepository.java
+++ Message-service/src/main/java/pl/polsl/krypczyk/apartmentsforrent/messageservice/domain/message/MessageRepository.java
@@ -3,6 +3,10 @@ package pl.polsl.krypczyk.apartmentsforrent.messageservice.domain.message;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
+
 @Repository
 public interface MessageRepository extends JpaRepository<MessageEntity, Long> {
+
+    Collection<MessageEntity> getMessageEntitiesBySenderIdAndReceiverId(Long senderId, Long ReceiverId);
 }
