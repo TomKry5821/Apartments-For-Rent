a136505976d9c4da966acc98084cb35c4b832c71
TomKry5821 <tomakry729@student.polsl.pl>
2022-10-08 17:48:10 +0200

    Added delete observed announcement listener

--- Announcement-service/src/main/java/pl/polsl/krypczyk/apartmentsforrent/announcementservice/application/announcement/AnnouncementListener.java
+++ Announcement-service/src/main/java/pl/polsl/krypczyk/apartmentsforrent/announcementservice/application/announcement/AnnouncementListener.java
@@ -11,9 +11,10 @@ public class AnnouncementListener {
 
     private final String GROUP_ID = "apartments-for-rent";
     private final String INACTIVATE_ANNOUNCEMENT_TOPIC = "inactivate-announcement";
-
     private final String DELETE_ANNOUNCEMENT_TOPIC = "delete-announcement";
+    private final String DELETE_OBSERVED_ANNOUNCEMENT_TOPIC = "delete-observed-announcement";
     private final AnnouncementService announcementService;
+
     // IF DATA WILL BE OUR CUSTOM CLASS WE WILL PASS THERE THAT CLASS
     @KafkaListener(topics = INACTIVATE_ANNOUNCEMENT_TOPIC, groupId = GROUP_ID, containerFactory = "kafkaListenerWithUserIdContainerFactory")
     void closeUserAnnouncementsListener(Long userId) {
@@ -23,5 +24,10 @@ public class AnnouncementListener {
     @KafkaListener(topics = DELETE_ANNOUNCEMENT_TOPIC, groupId = GROUP_ID, containerFactory = "kafkaListenerWithUserIdContainerFactory")
     void deleteUserAnnouncementsListener(Long userId) {
         this.announcementService.deleteUserAnnouncements(userId);
+    }
+
+    @KafkaListener(topics = DELETE_OBSERVED_ANNOUNCEMENT_TOPIC, groupId = GROUP_ID, containerFactory = "kafkaListenerWithUserIdContainerFactory")
+    void deleteObservedAnnouncementListener(Long userId) {
+        this.announcementService.deleteObservedAnnouncements(userId);
     }
 }
