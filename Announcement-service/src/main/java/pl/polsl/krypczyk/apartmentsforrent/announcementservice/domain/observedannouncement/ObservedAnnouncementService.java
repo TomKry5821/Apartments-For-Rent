61389e4a03b1877af822d060e0981daaa16ae529
TomKry5821 <tomakry729@student.polsl.pl>
2022-10-10 21:40:15 +0200

    Added get observed announcements method

--- Announcement-service/src/main/java/pl/polsl/krypczyk/apartmentsforrent/announcementservice/domain/observedannouncement/ObservedAnnouncementService.java
+++ Announcement-service/src/main/java/pl/polsl/krypczyk/apartmentsforrent/announcementservice/domain/observedannouncement/ObservedAnnouncementService.java
@@ -1,10 +1,13 @@
 package pl.polsl.krypczyk.apartmentsforrent.announcementservice.domain.observedannouncement;
 
 import pl.polsl.krypczyk.apartmentsforrent.announcementservice.application.announcement.response.ObserveAnnouncementResponse;
+import pl.polsl.krypczyk.apartmentsforrent.announcementservice.application.observedannouncement.dto.ObservedAnnouncementDTO;
 import pl.polsl.krypczyk.apartmentsforrent.announcementservice.domain.announcement.excpetion.AnnouncementNotFoundException;
 import pl.polsl.krypczyk.apartmentsforrent.announcementservice.domain.announcement.excpetion.ClosedAnnouncementException;
 import pl.polsl.krypczyk.apartmentsforrent.announcementservice.domain.announcement.excpetion.InvalidUserIdException;
 import pl.polsl.krypczyk.apartmentsforrent.announcementservice.domain.observedannouncement.exception.AnnouncementAlreadyObservedException;
+
+import java.util.Collection;
 
 public interface ObservedAnnouncementService {
 
@@ -13,5 +16,7 @@ public interface ObservedAnnouncementService {
     void unobserveAnnouncement(Long announcementId, Long userId, Long requesterId) throws InvalidUserIdException, AnnouncementNotFoundException;
 
     void deleteObservedAnnouncements(Long userId);
+
+    Collection<ObservedAnnouncementDTO> getObservedAnnouncements(Long userId, Long requesterId) throws InvalidUserIdException;
 
 }
