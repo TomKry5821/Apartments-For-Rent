4aab451dd54e75d8c487f80fe123f7faae3ba2c6
TomKry5821 <tomakry729@student.polsl.pl>
2022-10-10 21:39:46 +0200

    Added get observed announcements method

---
+++ Announcement-service/src/main/java/pl/polsl/krypczyk/apartmentsforrent/announcementservice/application/observedannouncement/dto/ObservedAnnouncementDTO.java
@@ -0,0 +1,14 @@
+package pl.polsl.krypczyk.apartmentsforrent.announcementservice.application.observedannouncement.dto;
+
+import lombok.Data;
+
+@Data
+public class ObservedAnnouncementDTO {
+
+    private String title;
+
+    private String mainPhotoPath;
+
+    private Long userId;
+
+}
