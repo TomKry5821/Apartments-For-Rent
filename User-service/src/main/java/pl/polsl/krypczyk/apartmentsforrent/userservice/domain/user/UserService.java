32234108125dd592747af7f19ea769572cbae5e6
TomKry5821 <tomakry729@student.polsl.pl>
2022-10-14 22:48:27 +0200

    Added create user method method

--- User-service/src/main/java/pl/polsl/krypczyk/apartmentsforrent/userservice/domain/user/UserService.java
+++ User-service/src/main/java/pl/polsl/krypczyk/apartmentsforrent/userservice/domain/user/UserService.java
@@ -3,11 +3,16 @@ package pl.polsl.krypczyk.apartmentsforrent.userservice.domain.user;
 import pl.polsl.krypczyk.apartmentsforrent.userservice.application.security.userdetails.request.ChangeUserDetailsRequest;
 import pl.polsl.krypczyk.apartmentsforrent.userservice.application.security.userdetails.response.ChangeUserDetailsResponse;
 import pl.polsl.krypczyk.apartmentsforrent.userservice.application.security.userdetails.response.GetUserDetailsResponse;
+import pl.polsl.krypczyk.apartmentsforrent.userservice.application.user.request.CreateUserRequest;
+import pl.polsl.krypczyk.apartmentsforrent.userservice.application.user.response.CreateUserResponse;
+import pl.polsl.krypczyk.apartmentsforrent.userservice.domain.security.exception.BadCredentialsException;
 import pl.polsl.krypczyk.apartmentsforrent.userservice.domain.user.exception.InvalidUserDetailsException;
 import pl.polsl.krypczyk.apartmentsforrent.userservice.domain.user.exception.UserAlreadyExistsException;
 import pl.polsl.krypczyk.apartmentsforrent.userservice.domain.user.exception.UserNotFoundException;
 
 public interface UserService {
+
+    CreateUserResponse createUser(CreateUserRequest createUserRequest) throws BadCredentialsException, UserAlreadyExistsException;
 
     GetUserDetailsResponse getUserDetails(Long userId) throws UserNotFoundException;
 
