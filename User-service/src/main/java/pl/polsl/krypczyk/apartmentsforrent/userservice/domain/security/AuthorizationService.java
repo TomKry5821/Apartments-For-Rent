ac2f0deb5c734cf6619830cb7db0169c8262398c
TomKry5821 <tomakry729@student.polsl.pl>
2022-10-14 22:47:51 +0200

    Removed create user method

--- User-service/src/main/java/pl/polsl/krypczyk/apartmentsforrent/userservice/domain/security/AuthorizationService.java
+++ User-service/src/main/java/pl/polsl/krypczyk/apartmentsforrent/userservice/domain/security/AuthorizationService.java
@@ -1,17 +1,12 @@
 package pl.polsl.krypczyk.apartmentsforrent.userservice.domain.security;
 
-import pl.polsl.krypczyk.apartmentsforrent.userservice.application.user.request.CreateUserRequest;
 import pl.polsl.krypczyk.apartmentsforrent.userservice.application.user.request.UserLoginRequest;
-import pl.polsl.krypczyk.apartmentsforrent.userservice.application.user.response.CreateUserResponse;
 import pl.polsl.krypczyk.apartmentsforrent.userservice.application.user.response.LoginUserResponse;
 import pl.polsl.krypczyk.apartmentsforrent.userservice.domain.security.exception.BadCredentialsException;
 import pl.polsl.krypczyk.apartmentsforrent.userservice.domain.security.exception.UnauthorizedUserException;
-import pl.polsl.krypczyk.apartmentsforrent.userservice.domain.user.exception.UserAlreadyExistsException;
 import pl.polsl.krypczyk.apartmentsforrent.userservice.domain.user.exception.UserNotFoundException;
 
 public interface AuthorizationService {
-
-    CreateUserResponse registerNewUser(CreateUserRequest createUserRequest) throws BadCredentialsException, UserAlreadyExistsException;
 
     LoginUserResponse loginUser(UserLoginRequest userLoginRequest) throws BadCredentialsException, UserNotFoundException;
 
