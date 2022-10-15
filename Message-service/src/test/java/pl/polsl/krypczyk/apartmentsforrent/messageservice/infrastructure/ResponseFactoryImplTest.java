67ad841b21f435028c3bf3fce91f6ddd1e3e52cd
TomKry5821 <tomakry729@student.polsl.pl>
2022-10-15 15:36:00 +0200

    Added tests

---
+++ Message-service/src/test/java/pl/polsl/krypczyk/apartmentsforrent/messageservice/infrastructure/ResponseFactoryImplTest.java
@@ -0,0 +1,18 @@
+package pl.polsl.krypczyk.apartmentsforrent.messageservice.infrastructure;
+
+import org.junit.jupiter.api.Test;
+
+
+class ResponseFactoryImplTest {
+
+    @Test
+    void createAddNewMessageResponse() {
+        //GIVEN
+    }
+
+    @Test
+    void createGetConversationResponse() {
+    }
+
+
+}
