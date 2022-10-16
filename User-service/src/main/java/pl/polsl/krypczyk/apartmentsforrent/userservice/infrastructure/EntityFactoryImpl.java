e9b366f6dda7321257f256dd26e428d8971dba2d
TomKry5821 <tomakry729@student.polsl.pl>
2022-10-16 17:55:14 +0200

    Cleanup

--- User-service/src/main/java/pl/polsl/krypczyk/apartmentsforrent/userservice/infrastructure/EntityFactoryImpl.java
+++ User-service/src/main/java/pl/polsl/krypczyk/apartmentsforrent/userservice/infrastructure/EntityFactoryImpl.java
@@ -3,18 +3,15 @@ package pl.polsl.krypczyk.apartmentsforrent.userservice.infrastructure;
 import lombok.RequiredArgsConstructor;
 import org.mapstruct.factory.Mappers;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 import pl.polsl.krypczyk.apartmentsforrent.userservice.infrastructure.security.config.AES;
 import pl.polsl.krypczyk.apartmentsforrent.userservice.application.user.request.CreateUserRequest;
 import pl.polsl.krypczyk.apartmentsforrent.userservice.domain.EntityFactory;
 import pl.polsl.krypczyk.apartmentsforrent.userservice.domain.role.RoleEntity;
-import pl.polsl.krypczyk.apartmentsforrent.userservice.domain.role.RoleRepository;
 import pl.polsl.krypczyk.apartmentsforrent.userservice.domain.user.UserEntity;
 import pl.polsl.krypczyk.apartmentsforrent.userservice.domain.user.UserMapper;
-import pl.polsl.krypczyk.apartmentsforrent.userservice.domain.user.UserRepository;
 import pl.polsl.krypczyk.apartmentsforrent.userservice.domain.userauthorization.UserAuthorizationEntity;
-import pl.polsl.krypczyk.apartmentsforrent.userservice.domain.userauthorization.UserAuthorizationRepository;
 import pl.polsl.krypczyk.apartmentsforrent.userservice.domain.userdetails.UserDetailsEntity;
-import pl.polsl.krypczyk.apartmentsforrent.userservice.domain.userdetails.UserDetailsRepository;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -22,19 +19,14 @@ import java.util.UUID;
 
 @Component
 @RequiredArgsConstructor
+@Transactional
 public class EntityFactoryImpl implements EntityFactory {
-
-    private final UserRepository userRepository;
-    private final UserDetailsRepository userDetailsRepository;
-    private final UserAuthorizationRepository userAuthorizationRepository;
-    private final RoleRepository roleRepository;
     private final UserMapper userMapper = Mappers.getMapper(UserMapper.class);
 
 
     @Override
     public RoleEntity createUserRoleEntity() {
-        var role = new RoleEntity("ROLE_USER");
-        return this.roleRepository.save(role);
+        return new RoleEntity("ROLE_USER");
     }
 
     @Override
@@ -42,7 +34,7 @@ public class EntityFactoryImpl implements EntityFactory {
         var userAuthorization = new UserAuthorizationEntity();
         userAuthorization.setToken(UUID.randomUUID());
         userAuthorization.setRoles(List.of(roleEntity));
-        return this.userAuthorizationRepository.save(userAuthorization);
+        return userAuthorization;
     }
 
     @Override
@@ -50,7 +42,7 @@ public class EntityFactoryImpl implements EntityFactory {
         var userDetails = this.userMapper.userDetailsDTOToUserDetailsEntity(createUserRequest);
         userDetails.setCreationDate(LocalDateTime.now());
         userDetails.setPassword(AES.encrypt(userDetails.getPassword()));
-        return this.userDetailsRepository.save(userDetails);
+        return userDetails;
     }
 
     @Override
@@ -58,6 +50,6 @@ public class EntityFactoryImpl implements EntityFactory {
         var userEntity = new UserEntity();
         userEntity.setUserDetailsEntity(userDetailsEntity);
         userEntity.setUserAuthorizationEntity(userAuthorizationEntity);
-        return userRepository.save(userEntity);
+        return userEntity;
     }
 }
