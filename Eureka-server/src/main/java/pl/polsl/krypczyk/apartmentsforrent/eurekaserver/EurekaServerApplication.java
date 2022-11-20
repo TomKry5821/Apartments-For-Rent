29461cf40aee2a6c0e7e00da9ab92726d57d050a
TomKry5821 <tomakry729@student.polsl.pl>
2022-11-20 15:01:30 +0100

    Added Eureka server project

---
+++ Eureka-server/src/main/java/pl/polsl/krypczyk/apartmentsforrent/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,13 @@
+package pl.polsl.krypczyk.apartmentsforrent.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
