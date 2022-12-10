e2f6697cc3fd72b464c7aa40393eab7dbb9cca67
TomKry5821 <tomakry729@student.polsl.pl>
2022-12-10 01:22:39 +0100

    Added get all receivers by sender id method

--- Message-service/src/main/java/pl/polsl/krypczyk/apartmentsforrent/messageservice/domain/message/MessageRepository.java
+++ Message-service/src/main/java/pl/polsl/krypczyk/apartmentsforrent/messageservice/domain/message/MessageRepository.java
@@ -1,12 +1,15 @@
 package pl.polsl.krypczyk.apartmentsforrent.messageservice.domain.message;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Collection;
 
 @Repository
 public interface MessageRepository extends JpaRepository<MessageEntity, Long> {
-
     Collection<MessageEntity> getMessageEntitiesBySenderIdAndReceiverId(Long senderId, Long ReceiverId);
+
+    @Query("select distinct m.receiverId from MessageEntity m where m.senderId = ?1")
+    Collection<Long> getAllReceiversBySenderId(Long senderId);
 }
